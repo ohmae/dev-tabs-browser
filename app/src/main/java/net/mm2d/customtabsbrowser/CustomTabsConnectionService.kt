00c65cd916117e49b8a5af9c04234747b100cf7b
OHMAE Ryosuke <ryo@mm2d.net>
2018-10-28 18:57:16 +0900

    :recycle: reorder

--- app/src/main/java/net/mm2d/customtabsbrowser/CustomTabsConnectionService.kt
+++ app/src/main/java/net/mm2d/customtabsbrowser/CustomTabsConnectionService.kt
@@ -16,45 +16,12 @@ import androidx.browser.customtabs.CustomTabsSessionToken
  * @author [大前良介 (OHMAE Ryosuke)](mailto:ryo@mm2d.net)
  */
 class CustomTabsConnectionService : CustomTabsService() {
-    override fun warmup(flags: Long): Boolean {
-        return true
-    }
-
-    override fun newSession(sessionToken: CustomTabsSessionToken?): Boolean {
-        return true
-    }
-
-    override fun mayLaunchUrl(
-            sessionToken: CustomTabsSessionToken?,
-            url: Uri?,
-            extras: Bundle?,
-            otherLikelyBundles: MutableList<Bundle>?
-    ): Boolean {
-        return true
-    }
-
-    override fun extraCommand(commandName: String?, args: Bundle?): Bundle? {
-        return null
-    }
-
-    override fun requestPostMessageChannel(sessionToken: CustomTabsSessionToken?, postMessageOrigin: Uri?): Boolean {
-        return false
-    }
-
-    override fun postMessage(sessionToken: CustomTabsSessionToken?, message: String?, extras: Bundle?): Int {
-        return CustomTabsService.RESULT_FAILURE_DISALLOWED
-    }
-
-    override fun validateRelationship(
-            sessionToken: CustomTabsSessionToken?,
-            relation: Int,
-            origin: Uri?,
-            extras: Bundle?
-    ): Boolean {
-        return false
-    }
-
-    override fun updateVisuals(sessionToken: CustomTabsSessionToken?, bundle: Bundle?): Boolean {
-        return false
-    }
+    override fun warmup(flags: Long): Boolean = false
+    override fun newSession(sessionToken: CustomTabsSessionToken?): Boolean = true
+    override fun mayLaunchUrl(sessionToken: CustomTabsSessionToken?, url: Uri?, extras: Bundle?, otherLikelyBundles: MutableList<Bundle>?): Boolean = true
+    override fun extraCommand(commandName: String?, args: Bundle?): Bundle? = null
+    override fun requestPostMessageChannel(sessionToken: CustomTabsSessionToken?, postMessageOrigin: Uri?): Boolean = false
+    override fun postMessage(sessionToken: CustomTabsSessionToken?, message: String?, extras: Bundle?): Int = CustomTabsService.RESULT_FAILURE_DISALLOWED
+    override fun validateRelationship(sessionToken: CustomTabsSessionToken?, relation: Int, origin: Uri?, extras: Bundle?): Boolean = false
+    override fun updateVisuals(sessionToken: CustomTabsSessionToken?, bundle: Bundle?): Boolean = false
 }
