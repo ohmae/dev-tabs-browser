a309e4f83804f2274afa3f8e11b4ba8ae20b2830
OHMAE Ryosuke <ryo@mm2d.net>
2020-02-12 21:15:43 +0900

    ♻use runCatching

--- app/src/main/java/net/mm2d/customtabsbrowser/IntentUtils.kt
+++ app/src/main/java/net/mm2d/customtabsbrowser/IntentUtils.kt
@@ -14,86 +14,40 @@ import android.os.Parcelable
 /**
  * @author [大前良介 (OHMAE Ryosuke)](mailto:ryo@mm2d.net)
  */
-internal fun Intent.getBooleanExtraSafely(key: String, default: Boolean = false): Boolean {
-    return try {
-        getBooleanExtra(key, default)
-    } catch (e: Exception) {
-        default
-    }
-}
+internal fun Intent.getBooleanExtraSafely(key: String, default: Boolean = false): Boolean =
+    runCatching { getBooleanExtra(key, default) }.getOrDefault(default)
 
-internal fun Intent.getIntExtraSafely(key: String, default: Int = 0): Int {
-    return try {
-        getIntExtra(key, default)
-    } catch (e: Exception) {
-        default
-    }
-}
+internal fun Intent.getIntExtraSafely(key: String, default: Int = 0): Int =
+    runCatching { getIntExtra(key, default) }.getOrDefault(default)
 
-internal fun Intent.getIntArrayExtraSafely(key: String): IntArray? {
-    return try {
-        getIntArrayExtra(key)
-    } catch (e: Exception) {
-        null
-    }
-}
+internal fun Intent.getIntArrayExtraSafely(key: String): IntArray? =
+    runCatching { getIntArrayExtra(key) }.getOrNull()
 
-internal fun Intent.getBundleExtraSafely(key: String): Bundle? {
-    return try {
-        getBundleExtra(key)
-    } catch (e: Exception) {
-        null
-    }
-}
+internal fun Intent.getBundleExtraSafely(key: String): Bundle? =
+    runCatching { getBundleExtra(key) }.getOrNull()
 
-internal fun <T : Parcelable> Intent.getParcelableExtraSafely(key: String): T? {
-    return try {
-        getParcelableExtra(key)
-    } catch (e: Exception) {
-        null
-    }
-}
+internal fun <T : Parcelable> Intent.getParcelableExtraSafely(key: String): T? =
+    runCatching { getParcelableExtra<T>(key) }.getOrNull()
 
-internal fun <T : Parcelable> Intent.getParcelableArrayListExtraSafely(key: String): ArrayList<T>? {
-    return try {
-        getParcelableArrayListExtra(key)
-    } catch (e: Exception) {
-        null
-    }
-}
+internal fun <T : Parcelable> Intent.getParcelableArrayListExtraSafely(key: String): ArrayList<T>? =
+    runCatching { getParcelableArrayListExtra<T>(key) }.getOrNull()
 
 internal fun Bundle?.getIntSafely(key: String, default: Int = 0): Int {
     this ?: return default
-    return try {
-        getInt(key)
-    } catch (e: Exception) {
-        default
-    }
+    return runCatching { getInt(key) }.getOrDefault(default)
 }
 
 internal fun Bundle?.getStringSafely(key: String, default: String? = null): String? {
     this ?: return default
-    return try {
-        getString(key) ?: default
-    } catch (e: Exception) {
-        default
-    }
+    return runCatching { getString(key) }.getOrNull() ?: default
 }
 
 internal fun Bundle?.getStringSafelyNonNull(key: String, default: String = ""): String {
     this ?: return default
-    return try {
-        getString(key) ?: default
-    } catch (e: Exception) {
-        default
-    }
+    return runCatching { getString(key) }.getOrNull() ?: default
 }
 
 internal fun <T : Parcelable> Bundle?.getParcelableSafely(key: String, default: T? = null): T? {
     this ?: return default
-    return try {
-        getParcelable(key)
-    } catch (e: Exception) {
-        null
-    }
+    return runCatching { getParcelable<T>(key) }.getOrNull() ?: default
 }
