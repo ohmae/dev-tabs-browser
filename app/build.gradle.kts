0f14457266303980ca87b26b532cf1eed72a9445
OHMAE Ryosuke <ryo@mm2d.net>
2020-10-06 20:18:23 +0900

    🆙kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,9 @@ android {
         jvmTarget = "1.8"
     }
     buildTypes {
+        getByName("debug") {
+            isDebuggable = true
+        }
         getByName("release") {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
@@ -36,16 +39,16 @@ android {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.0")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.0")
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.1")
     implementation("androidx.recyclerview:recyclerview:1.1.0")
-    implementation("androidx.core:core-ktx:1.3.1")
+    implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.browser:browser:1.2.0")
-    implementation("com.google.android.material:material:1.2.0")
+    implementation("com.google.android.material:material:1.2.1")
     testImplementation("junit:junit:4.13")
-    testImplementation("org.robolectric:robolectric:4.3.1")
+    testImplementation("org.robolectric:robolectric:4.4")
 }
 
 fun isStable(version: String): Boolean {
